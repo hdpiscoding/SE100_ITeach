c1fc2339cbf7049d7b12fd9a1c72fc9b98994cef
KKuyen <22521225@gm.uit.edu.vn>
2024-09-24 17:01:04 +0700

    chinh db

---
+++ backend/src/config/config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: true,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: true,
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
