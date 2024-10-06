19179eaa3d3c10f2dcd9b30b77a48b0be376fbef
PhamKhaiHung <22520522@gm.uit.edu.vn>
2024-10-07 00:59:55 +0700

    add lib

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Narbar from './components/Navbar/Narbar'
+
+const App = () => {
+  return (
+    <div>
+        <Narbar />
+    </div>
+  )
+}
+
+export default App
