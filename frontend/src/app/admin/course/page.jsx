13580e1e9da3ff246ff485e26659ac0c139873db
PhamKhaiHung <22520522@gm.uit.edu.vn>
2024-12-18 20:27:54 +0700

    sua course admin

--- frontend/src/app/admin/course/page.jsx
+++ frontend/src/app/admin/course/page.jsx
@@ -12,65 +12,78 @@ import {
 } from "@/components/ui/select";
 
 const CoursesAdmin = () => {
+  const [activeTab, setActiveTab] = useState("public");
   const [currentPage, setCurrentPage] = useState(1);
   const coursesPerPage = 12;
 
-  
-  const courses = new Array(17).fill(null); 
-
+  const courses = new Array(17).fill(null);
 
   const indexOfLastCourse = currentPage * coursesPerPage;
   const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
   const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
 
-  
   const totalPages = Math.ceil(courses.length / coursesPerPage);
-
 
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
   return (
     <div className="space-y-7">
-        
-        <div className="h-[120px] bg-bg grid grid-cols-[0.5fr_11fr_0.5fr]">
-       
-       <div></div>
-       <div className="flex items-center">
-         <div className="space-x-2 ">
-       
-           <Image
-             className="inline-block"
-             src="/assets/images/home.png"
-             alt="banner"
-             width={20}
-             height={20}
-           />
-           <Image
-             className="inline-block"
-             src="/assets/images/arrow_right.png"
-             alt="banner"
-             width={5}
-             height={5}
-           />
-           <span className="text-orange font-bold"> Khóa học</span>
-        
-        
-       </div>
-       </div>
-     <div></div>
-   </div>
+      <div className="h-[120px] bg-bg grid grid-cols-[0.5fr_11fr_0.5fr]">
+        <div></div>
+        <div className="flex items-center">
+          <div className="space-x-2 ">
+            <Image
+              className="inline-block"
+              src="/assets/images/home.png"
+              alt="banner"
+              width={20}
+              height={20}
+            />
+            <Image
+              className="inline-block"
+              src="/assets/images/arrow_right.png"
+              alt="banner"
+              width={5}
+              height={5}
+            />
+            <span className="text-orange font-bold"> Khóa học</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
       <div className="grid grid-cols-[0.5fr_11fr_0.5fr] ">
         <div></div>
-        
+
         <div className="space-y-7 my-10">
-            <div className="flex justify-center space-x-10 lg:text-xl md:text-lg sm:text-sm text-xs">
-                <span className="">Khóa học công khai</span>
-                <span className=" text-orange">Khóa học chờ duyệt(99)</span>
-                <span className=" ">Khóa học tạm ngưng</span>
-            </div>
+          <div className="flex justify-center space-x-10 lg:text-xl md:text-lg sm:text-sm text-xs">
+            <span
+              className={`cursor-pointer ${
+                activeTab === "public" ? "text-orange" : ""
+              }`}
+              onClick={() => setActiveTab("public")}
+            >
+              Khóa học công khai
+            </span>
+            <span
+              className={`cursor-pointer ${
+                activeTab === "pending" ? "text-orange" : ""
+              }`}
+              onClick={() => setActiveTab("pending")}
+            >
+              Khóa học chờ duyệt(99)
+            </span>
+            <span
+              className={`cursor-pointer ${
+                activeTab === "paused" ? "text-orange" : ""
+              }`}
+              onClick={() => setActiveTab("paused")}
+            >
+              Khóa học tạm ngưng
+            </span>
+          </div>
           <div className="flex justify-between">
             <div className="flex lg:flex-row flex-col lg:space-x-5 space-y-3 lg:space-y-0">
-            <div className="rounded-3xl border-filter border-2 lg:w-[300px] md:w-[250px] sm:w-[230px] w-[200px] flex justify-between items-center p-1">
+              <div className="rounded-3xl border-filter border-2 lg:w-[300px] md:w-[250px] sm:w-[230px] w-[200px] flex justify-between items-center p-1">
                 <input
                   className="outline-none lg:w-[270px] md:w-[220px] sm:w-[180px] w-[150px] lg:text-base md:text-sm sm:text-xs text-xs rounded-3xl"
                   type="text"
@@ -87,14 +100,19 @@ const CoursesAdmin = () => {
                 </div>
               </div>
               <div className="  grid grid-cols-[1fr_2fr] gap-6 ">
-                <span className="md:text-base sm:text-sm lg:text-xl text-xs flex items-center">Sort by:</span>
+                <span className="md:text-base sm:text-sm lg:text-xl text-xs flex items-center">
+                  Sort by:
+                </span>
                 <Select className="md:text-base sm:text-sm lg:text-xl text-xs">
                   <SelectTrigger className="lg:w-[180px] md:w-[150px] sm:w-[120px] w-[100px]">
-                    <SelectValue className="lg:text-xl md:text-lg sm:text-sm text-xs" placeholder="Most popular" />
+                    <SelectValue
+                      className="lg:text-xl md:text-lg sm:text-sm text-xs"
+                      placeholder="Most popular"
+                    />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectItem value="light">Light</SelectItem>
-                    <SelectItem value="dark">Dark</SelectItem>  
+                    <SelectItem value="dark">Dark</SelectItem>
                     <SelectItem value="system">System</SelectItem>
                   </SelectContent>
                 </Select>
@@ -127,14 +145,18 @@ const CoursesAdmin = () => {
                 key={number}
                 onClick={() => setCurrentPage(number)}
                 className={`px-4 py-2 rounded ${
-                  currentPage === number ? "bg-white text-strokeswap border border-strokeswap" : "bg-gray-200"
+                  currentPage === number
+                    ? "bg-white text-strokeswap border border-strokeswap"
+                    : "bg-gray-200"
                 }`}
               >
                 {number}
               </button>
             ))}
             <button
-              onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+              onClick={() =>
+                setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+              }
               disabled={currentPage === totalPages}
               className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50"
             >
