7620b47710faa5b38e6bebf332740b34ff366edf
NDHunq <22520514@gm.uit.edu.vn>
2024-12-18 20:11:06 +0700

    sua

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import SignIn from "@/components/SignIn";
 const Navbar = () => {
   const pathname = usePathname();
   const router = useRouter();
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [role, setRole] = useState("student");
   const [showLoginModal, setShowLoginModal] = useState(false);
