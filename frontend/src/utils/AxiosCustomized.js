0674a24c5b77dc49303e564db193e0c2e47eead3
hdpiscoding <22520339@gm.uit.edu.vn>
2025-01-07 00:09:48 +0700

    change axios base url

--- frontend/src/utils/AxiosCustomized.js
+++ frontend/src/utils/AxiosCustomized.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://iteach-d4vn.onrender.com/",
   timeout: 70000,
   headers: {
     "Content-Type": "application/json; charset=utf-8",
