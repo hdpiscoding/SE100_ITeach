433dc4c05e5c4fe4eae25791b0a67b5153356101
PhamKhaiHung <22520522@gm.uit.edu.vn>
2025-01-07 18:23:31 +0700

    sua lai port 8080

--- frontend/src/utils/AxiosCustomized.js
+++ frontend/src/utils/AxiosCustomized.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:7777/",
+  baseURL: "http://localhost:8080/",
   timeout: 70000,
   headers: {
     "Content-Type": "application/json; charset=utf-8",
